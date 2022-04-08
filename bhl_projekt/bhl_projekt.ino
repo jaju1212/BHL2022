155d2238ca5fbe4926287daa8deba43417e070dd
jaju1212 <80427858+jaju1212@users.noreply.github.com>
2022-04-08 18:54:53 +0200

    Update bhl_projekt.ino

--- bhl_projekt/bhl_projekt.ino
+++ bhl_projekt/bhl_projekt.ino
@@ -1,33 +1,54 @@
-#include <LiquidCrystal.h>
-
+#include<Servo.h>
 
 #define PIN A0
+#define STEP_PIN 9
+#define DIR_PIN 10
+
 #define PILL_TAKEN_PIN 12
 #define REMINDER_DIODE 10
 
+#define STEP_DEG 4.5
 
 void setup() {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
-  
+
 }
 
 void loop() {
-  int adc = digitalRead(PIN);
-  delay(100);
-  Serial.println(adc);
   
-  
+  delay(1000);
+
+
 }
 
 //Function prototypes
 
-bool checkIfTaken(void){
+bool checkIfTaken(void) {
   bool val = digitalRead(PILL_TAKEN_PIN);
   return val;
 }
 
-void reminderDiodeState(int state){
+void reminderDiodeState(int state) {
   digitalWrite(REMINDER_DIODE, state);
+}
+
+void stepperAngle(int pulse, int dir) {
+  if (dir == 0) {
+    digitalWrite(STEP_PIN, LOW);
+  }
+  else
+  {
+    digitalWrite(STEP_PIN, HIGH);
+  }
+
+  for(int i = 0; i < pulse; i++){
+    digitalWrite(STEP_PIN, HIGH);
+    delay(1);
+    digitalWrite(STEP_PIN, LOW);
+  }
+
+
+
 }
